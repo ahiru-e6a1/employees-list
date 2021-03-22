cd79af531b38b9bf5a40fbadb76cf6b8d3499214
ahiru-e6a1 <a.h.i.r.u@icloud.com>
2021-03-22 23:53:53 +0900

    カード作成

--- pages/employee-page.js
+++ pages/employee-page.js
@@ -1,12 +1,31 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 
-export default function Employee() {
+const Employee = () => {
   return (
     <Layout title="Employee">
-      <div className="bg-white text-center shodow-xl p-8 w-80 rounded">
-        <div className="font-bold">Employee Page</div>
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4 pr-10">
+          <p className="font-bold">No.1</p>
+        </div>
+        <div className="flex">
+          <div className="mt-2 pr-8">
+            <Image
+              className="rounded-full"
+              src="/cat-1.jpg"
+              width={60}
+              height={60}
+              alt="Avatar"
+            />
+          </div>
+          <div className="mt-4">
+            <p className="text-xs mt-2 text-gray-600">enginer</p>
+            <p className="font-bold">taro suzuki</p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
-}
+};
+
+export default Employee;
